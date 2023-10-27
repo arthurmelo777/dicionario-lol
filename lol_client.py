69e1867acfc8bdca548f28584a803610b3f970e6
ArthurMelo777 <meloarthurmelo@gmail.com>
2023-10-27 15:24:48 -0300

    atividade DSD sobre RMI

---
+++ lol_client.py
@@ -0,0 +1,13 @@
+import Pyro4
+
+def main():
+    with Pyro4.Proxy("PYRONAME:lol_server") as server:
+        while True:
+            role = input("Digite uma rota (top, jungle, mid, adc, sup) ou 'exit' para sair: ")
+            if role == 'exit':
+                break
+            champions = server.get_champions_by_role(role)
+            print(f"Campeões da rota {role}: {', '.join(champions)}")
+
+if __name__ == "__main__":
+    main()
---
+++ lol_server.py
@@ -0,0 +1,30 @@
+import Pyro4
+
+champions = {
+    'top': ['Aatrox', 'Camille', 'Garen', 'Irelia'],
+    'jungle': ['Amumu', 'Evelynn', 'Graves', 'Lee Sin'],
+    'mid': ['Ahri', 'Annie', 'Orianna', 'Syndra'],
+    'adc': ['Ashe', 'Ezreal', 'Jhin', 'Miss Fortune'],
+    'sup': ['Braum', 'Leona', 'Morgana', 'Thresh'],
+}
+
+@Pyro4.expose
+class LOLServer(object):
+    def get_champions_by_role(self, role):
+        if role in champions:
+            return champions[role]
+        else:
+            return "Rota não encontrada"
+
+def main():
+    Pyro4.Daemon.serveSimple(
+        {
+            LOLServer: "lol_server"
+        },
+        host="localhost",
+        port=9090,
+        ns=False
+    )
+
+if __name__ == "__main__":
+    main()
